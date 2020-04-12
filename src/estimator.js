394d1b5a46c6d04064f47c9a88592030f230241c
sanniabdullateef <slateef39@gmail.com>
2020-04-12 08:33:39 +0100

    starting my project

--- src/estimator.js
+++ src/estimator.js
@@ -1,28 +1,42 @@
 // Default Configuration given
-const covid19ImpactEstimator = (data = defaultConfig) => {
+const input = {
+        region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+        },
 
-    return {
-        data,
-        impact: {
-            currentlyInfected: reportedCases * 10,
-            infectionsByRequestedTime: currentlyInfected * (Math.pow(2, (Math.floor( timeToElapse/3)))),
+        periodType: "days",
+        timeToElapse: 58,
+        reportedCases: 674,
+        population: 66622705,
+        totalHospitalBeds: 1380614
+};
+const covid19ImpactEstimator = (data=input) => ({
+
+        data: input,
+        estimate:{
+            impact: {
+            currentlyInfected: input.reportedCases * 10,
+            infectionsByRequestedTime: currentlyInfected * (2 ** (Math.floor( input.timeToElapse/3))),
             severeCasesByRequestedTime: 0.15 * infectionsByRequestedTime,
-            hospitalBedsByRequestedTime: totalHospitalBeds * 0.35,
+            hospitalBedsByRequestedTime: input.totalHospitalBeds * 0.35,
             casesForICUByRequestedTime: infectionsByRequestedTime * 0.5,
             casesForVentilatorsByRequestedTime: infectionsByRequestedTime * 0.2,
-            dollarsInFlight: (infectionsByRequestedTime * 0.65 * Math.floor(avgDailyIncomeInUSD)) / 30,
+            dollarsInFlight: (infectionsByRequestedTime * 0.65 * Math.floor(input.avgDailyIncomeInUSD)) / 30,
         },
 
         severeImpact: {
-            currentlyInfected: reportedCases * 50,
-            infectionsByRequestedTime: currentlyInfected * (Math.pow(2, (Math.floor(timeToElapse / 3)))),
+            currentlyInfected: input.reportedCases * 50,
+            infectionsByRequestedTime: currentlyInfected * (2 ** (Math.floor(input.timeToElapse / 3))),
             severeCasesByRequestedTime: 0.15 * infectionsByRequestedTime,
             hospitalBedsByRequestedTime: totalHospitalBeds * 0.35,
             casesForICUByRequestedTime: infectionsByRequestedTime * 0.5,
             casesForVentilatorsByRequestedTime: infectionsByRequestedTime * 0.2,
-            dollarsInFlight: (infectionsByRequestedTime * 0.65 * Math.floor(avgDailyIncomeInUSD)) / 30
-        }
+            dollarsInFlight: (infectionsByRequestedTime * 0.65 * Math.floor(input.avgDailyIncomeInUSD)) / 30
+        }, 
     }
-}
+});
 
 export default covid19ImpactEstimator;
