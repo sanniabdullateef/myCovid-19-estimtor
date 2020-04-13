55bb85392dc00c3ebf6fb4fbdf1b659ef9fd6d82
sanniabdullateef <slateef39@gmail.com>
2020-04-13 15:35:21 +0100

    problem1-3commit

--- src/estimator.js
+++ src/estimator.js
@@ -5,15 +5,15 @@ const input = {
         region: {
         name: "Africa",
         avgAge: 19.7,
-        avgDailyIncomeInUSD: 5,
-        avgDailyIncomePopulation: 0.71
+        avgDailyIncomeInUSD: 4,
+        avgDailyIncomePopulation: 0.73
         },
 
         periodType: "days",
-        timeToElapse: 58,
-        reportedCases: 674,
-        population: 66622705,
-        totalHospitalBeds: 1380614
+        timeToElapse: 38,
+        reportedCases: 2747,
+        population: 92931687,
+        totalHospitalBeds: 678874
 };
 const covid19ImpactEstimator = (data) => {
 const { reportedCases, periodType, totalHospitalBeds} = data;
