a0a13b03d0e2b2ec10d3d97535e3c2d3ecc89326
sanniabdullateef <slateef39@gmail.com>
2020-04-13 17:07:00 +0100

    coommit

--- src/estimator.js
+++ src/estimator.js
@@ -1,7 +1,7 @@
 // Default Configuration given below for input 
 
 // eslint-disable-next-line no-unused-vars
-const data = {
+const input = {
         region: {
         name: "Africa",
         avgAge: 19.7,
